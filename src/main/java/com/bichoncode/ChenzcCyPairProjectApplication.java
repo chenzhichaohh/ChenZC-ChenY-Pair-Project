783fbf757414cb7f86f3853ef5c91813f3c33304
BichonCode <chenzhichaohh@163.com>
2020-10-08 23:51:00 +0800

    项目构建及其初始化

---
+++ src/main/java/com/bichoncode/ChenzcCyPairProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bichoncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenzcCyPairProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChenzcCyPairProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bichoncode/ChenzcCyPairProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bichoncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChenzcCyPairProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("cy牛逼");
+    }
+
+}
